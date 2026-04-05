a9249b4c9e6fe1f7ed355eb420d94d565b9dea8b
DESKTOP-46SP1PP\sanghyun <shubbydobob@gmail.com>
2026-04-05 17:29:59 +0900

    아트워크 슬라이드 이미지와 아이덴티티 라벨 정리

--- components/home/services-overview.tsx
+++ components/home/services-overview.tsx
@@ -1,8 +1,9 @@
-import artWorkImage01 from "@/screen/KakaoTalk_20260405_165954830.jpg";
-import artWorkImage02 from "@/screen/KakaoTalk_20260405_165921726.jpg";
-import artWorkImage03 from "@/screen/KakaoTalk_20260405_165921726_01.jpg";
-import artWorkImage04 from "@/screen/KakaoTalk_20260405_165921726_02.jpg";
-import artWorkImage05 from "@/screen/KakaoTalk_20260405_165921726_03.jpg";
+import artWorkImage01 from "@/screen/KakaoTalk_20260405_172008179_01.jpg";
+import artWorkImage02 from "@/screen/KakaoTalk_20260405_172008179_02.jpg";
+import artWorkImage03 from "@/screen/KakaoTalk_20260405_172008179_03.jpg";
+import artWorkImage04 from "@/screen/KakaoTalk_20260405_172008179_04.jpg";
+import artWorkImage05 from "@/screen/KakaoTalk_20260405_172008179_05.jpg";
+import artWorkImage06 from "@/screen/KakaoTalk_20260405_172008179_06.jpg";
 
 import { ProfileSliderSection } from "@/components/home/profile-slider-section";
 import type { ShootService } from "@/types/content";
@@ -18,6 +19,7 @@ export function ServicesOverview({ services: _services }: ServicesOverviewProps)
     { image: artWorkImage03, title: "ART WORK 3", href: "/services" },
     { image: artWorkImage04, title: "ART WORK 4", href: "/services" },
     { image: artWorkImage05, title: "ART WORK 5", href: "/services" },
+    { image: artWorkImage06, title: "ART WORK 6", href: "/services" },
   ];
 
   return <ProfileSliderSection title="ART WORKS" items={sliderItems} hideDetails />;
